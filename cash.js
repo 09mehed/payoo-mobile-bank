ed85c66958817991d795b072c87454e343f92561
09mehed <foysalalmamun558@gmail.com>
2024-09-19 15:45:08 +0600

    full website

--- cash.js
+++ cash.js
@@ -1,16 +1,21 @@
-document.getElementById('cash-out').addEventListener('click', function(event){
+// step 1
+document.getElementById('cash-out').addEventListener('click', function (event){
     event.preventDefault();
-
+    // step 2
     const inputCashOut = document.getElementById('input-cash-out').value;
-    const inputBalance = parseInt(inputCashOut);
-    const cashOutPin = document.getElementById('cash-out-pin').value;
+    const inputNumber = parseInt(inputCashOut);
+    const pinNumber = document.getElementById('cash-out-pin').value;
 
-    if(cashOutPin === "1234"){
-        const balance = document.getElementById('account-balance').innerText;
+    // step 3
+    if(pinNumber === '1234'){
+        // step-4
+        const balance = document.getElementById('amount-balance').innerText;
+        // step- 5
         const balanceNumber = parseInt(balance);
-        const newBalance = balanceNumber - inputBalance;
-        document.getElementById('account-balance').innerText = newBalance;
+        const newBalance = balanceNumber - inputNumber;
+        // step- 6
+        document.getElementById('amount-balance').innerText = newBalance;
     }else{
-        alert('please try again');
+        console.log('please try again');
     }
 })
